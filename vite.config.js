14852026a357a71fecad8082f0fb5e25107960fe
gowthamthotakura <gowthamthotakura@gmail.com>
2024-08-20 18:56:56 -0400

    any message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/Developing-Front-End-Apps-with-React",
+    // "YOUR_REPOSITORY_NAME" above
   plugins: [react()],
 })
